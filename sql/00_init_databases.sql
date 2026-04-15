b45dc7009a9af24d50c367f5afc8022989b358f9
jayyyyte <138153456+jayyyyte@users.noreply.github.com>
2026-04-15 16:17:24 +0700

    update 00_init_databases.sql

--- sql/00_init_databases.sql
+++ sql/00_init_databases.sql
@@ -6,6 +6,9 @@
 -- =============================================================================
 
 -- Airflow metadata database
+CREATE DATABASE airflow_db;
+CREATE DATABASE metabaseappdb;
+
 SELECT 'CREATE DATABASE airflow_db'
 WHERE NOT EXISTS (
     SELECT FROM pg_database WHERE datname = 'airflow_db'
